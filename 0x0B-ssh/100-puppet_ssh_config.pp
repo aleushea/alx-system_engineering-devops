0e31dec824d3d31e12dbac002bdb3ddc15f4deff
aleushea <aleushea@gmail.com>
2024-01-26 03:23:01 -0800

    pupet as an option for config

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# puppet as an alternative to edit configuration file
+
+file { 'etc/ssh/ssh_config':
+	ensure  => present,
+content =>"
+	path    => '/etc/ssh/ssh_config',
+	host*
+	IdentityFile ~/.ssh/school
+	PasswordAuthentication no
+	",
